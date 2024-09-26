d8b2daadaa7f3f73adf780145450a943686cca18
kirill-kniazev <kirill.knzv@ya.ru>
2024-09-25 21:32:37 -0700

    removed test files

--- src/test.py
+++
@@ -1,6 +0,0 @@
-# get repo directory for future reference
-import os, sys
-from pathlib import Path
-repo_dir = Path(__file__).resolve().parent
-
-print (str(Path(__file__).resolve().parent / "c" / "get_images_parallel_high_speed_dynamic.dll"))
