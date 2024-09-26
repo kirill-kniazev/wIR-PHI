5e21a6a6eb6f86d91a056c99ee6aeca8a3ae90e0
kirill-kniazev <kirill.knzv@ya.ru>
2024-09-25 21:31:03 -0700

    Added file with CMOS camera control functions

---
+++ src/test.py
@@ -0,0 +1,6 @@
+# get repo directory for future reference
+import os, sys
+from pathlib import Path
+repo_dir = Path(__file__).resolve().parent
+
+print (str(Path(__file__).resolve().parent / "c" / "get_images_parallel_high_speed_dynamic.dll"))
